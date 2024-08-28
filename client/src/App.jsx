61b00b4d8f4327666d9ed5421c51a43d0ba00082
MBHunter88 <chellehunt88@gmail.com>
2024-08-28 00:14:45 -0400

    add user input and handleSubmit function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   const [city, setCity] = useState('Oakland'); //uses default city to render information
   const [weatherData, setWeatherData] = useState(null);
 
-  
-  useEffect(() => {
     const fetchWeather = async () => {
       try {
         const response = await fetch(`/api/weather?city=${city}`); //fetch data from server.js
@@ -19,14 +17,30 @@ function App() {
       }
     };
 
-    fetchWeather(); // Fetch weather data when city changes
-  }, [city]); //this useEffect runs whenever `city` changes
+ // useEffect to fetch data for default city on component mount
+ useEffect(() => {
+  fetchWeather(); // Fetch weather data for default city
+}, []); 
 
  //handle user input 
+const handleSubmit = (event) => {
+event.preventDefault()
+fetchWeather();
+}
 
- 
   return (
     <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder='Enter city here'
+        />
+     <button type="submit">Get Weather</button>
+      </form>
+    </div>
    {weatherData ? (
         <div>
           <h3>Weather in {weatherData.name}</h3>
