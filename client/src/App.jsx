b30858cf0f767b852ca3a5eaa68842621d201ea8
MBHunter88 <chellehunt88@gmail.com>
2024-08-29 00:02:13 -0400

    add loading state and fine tuned error handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,89 +9,93 @@ function App() {
   const [city, setCity] = useState('Oakley'); //renders data from the default state
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null); //add state for error handling
+  const [loading, setLoading] = useState(false) //state for loading indication
 
-//fetch weather api 
-    const fetchWeather = async () => {
+  //fetch weather api 
+  const fetchWeather = async () => {
 
-      //error handling for user input 
-      if(!city.trim()) {
-        setError('Please enter city name')
-        setWeatherData(null) //clear weather data
-        return
+    //error handling for user input 
+    if (!city.trim()) {
+      setError('Please enter city name') //error for empty input
+      setWeatherData(null) //clear weather data
+      return
+    }
+    setLoading(true); //start loading indication
+    setError(null); // clear any previous errors
+
+    try {
+      const response = await fetch(`/api/weather?city=${city}`); //fetch data from server.js
+
+      //error handling to check for response from server
+      if (!response.ok) { //if not ok (codes 200-299) throw error
+        throw new Error(`Error: ${response.status} ${response.statusText}`)
       }
 
-      try {
-        const response = await fetch(`/api/weather?city=${city}`); //fetch data from server.js
-       
-        //error handling to check for response from server
-        if(!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`)
-        }
-       
-        const data = await response.json();
+      const data = await response.json();
 
-        //error handling from openWeather API to check for valid city 
-        if (data.cod !== 200) {
-          throw new Error(data.message || 'Error fetching weather data');
-        }
-
-        setWeatherData(data); //updates weatherData from parsed data api request
-        setError(null) //clears previous error with succesful fetch
-
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-        setError(error.message ||'Failed to fetch weather data. Try again') //update setError to display error message
+      //error handling from openWeather API to check for valid city 
+      if (data.cod !== 200) {
+        throw new Error(data.message || 'Error fetching weather data');
       }
-    };
 
- // useEffect to fetch data for default city when component initially loads
- useEffect(() => {
-  fetchWeather(); // Fetch weather data for default city
-}, []); 
+      setWeatherData(data); //updates weatherData from parsed data api request
+      setError(null) //clears any current error with successful fetch
 
- //handle user input 
-const handleSubmit = (event) => {
-event.preventDefault() //prevents page from reloading
-fetchWeather(); //fetch data from user input
-}
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setError(error.message || 'Failed to fetch weather data. Try again') //update setError to display error message
+      setWeatherData(null) //clears data on error
+    } finally {
+      setLoading(false) //stop loading indicator
+    }
+  };
+
+  // useEffect to fetch data for default city when component initially loads
+  useEffect(() => {
+    fetchWeather(); // Fetch weather data for default city
+  }, []);
+
+  //handle user input 
+  const handleSubmit = (event) => {
+    event.preventDefault() //prevents page from reloading
+    fetchWeather(); //fetch data from user input
+  }
 
   return (
     <>
-    <div className='container'>
-    <div className='form'>
-      <form onSubmit={handleSubmit}>
-        <input
-        type="text"
-        onChange={(e) => setCity(e.target.value)}
-        placeholder='Enter city here'
-        autoFocus
-        />
-     <button type="submit">Get Weather</button>
-      </form>
-    </div>
+      <div className='container'>
+        <div className='form'>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              onChange={(e) => setCity(e.target.value)}
+              placeholder='Enter city here'
+              autoFocus
+            />
+            <button type="submit">Get Weather</button>
+          </form>
+        </div>
 
-    {error && <p className='error-msg'>{error}</p>}
+        {error && <p className='error-msg'>{error}</p>}
+        {loading && <p>Loading...</p>}
 
-   {weatherData ? (
-        <div className='weatherData'>
-           <div className='weatherDisplay'>
-          <h3>Weather in {weatherData.name}</h3>
-           <img
-            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-            // if icon isn't found default to description
-            alt={weatherData.weather[0].description} 
-          />
-         
-          <p>Temperature: {weatherData.main.temp}°F</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
-          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        {weatherData && (
+          <div className='weatherData'>
+            <div className='weatherDisplay'>
+              <h3>Weather in {weatherData.name}</h3>
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                // if icon isn't found default to description
+                alt={weatherData.weather[0].description}
+              />
+              <p>Temperature: {weatherData.main.temp}°F</p>
+              <p>Humidity: {weatherData.main.humidity}%</p>
+              <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+            </div>
           </div>
-        </div>
-      
-      ) : (
-        <p>Loading...</p>
-      )}
-        </div>
+
+        )}
+      </div>
     </>
   )
 }
